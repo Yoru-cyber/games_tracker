03d5b00c1e823f59512158ae02ce203ab85be6fa
Yoru-cyber <carlosmendez170210@gmail.com>
2025-01-29 22:53:42 -0400

    improve create form, added delete operation, added pagination for games, added dotenv to manage env files

--- games_tracker_app/urls.py
+++ games_tracker_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="tracker_home"),
-    path("create/game", views.create_game, name="create_game"),
+    path("games/new", views.create_game, name="create_game"),
+    path("games/", views.list_games, name="list_games"),
+    path("games/delete/<int:game_id>", views.delete_game, name="delete_game")
 ]
--- games_tracker_app/views.py
+++ games_tracker_app/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.http import HttpResponse, request
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
 
 from games_tracker_app.forms import GameForm
+from games_tracker_app.models import Game
 
 
 # Create your views here.
@@ -18,3 +20,18 @@ def create_game(request):
     else:
         form = GameForm()
     return render(request, "create_game.html", {"form": form})
+
+def list_games(request):
+    games = Game.objects.order_by('id').all()  # Fetch all items
+    paginator = Paginator(games, 10)  # Show 10 items per page
+    page_number = request.GET.get("page")  # Get the current page number from the URL
+    page_obj = paginator.get_page(
+        page_number
+    )  # Get the Page object for the current page
+    return render(request, "list_games.html", {"page_obj": page_obj})
+
+def delete_game(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    if request.method == "POST":
+        game.delete()
+        return redirect("list_games")
